bf22ad11b6ce9d352b61ede20a3245b9331c63c6
Zach Holloway <zholloway10@outlook.com>
2024-05-21 00:35:43 -0500

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["api-id"] = "PKAJ4LWN30B9U3AB4N0F"
-os.environ["api-secret"] = "e5AhAfBnunoLbp8cYOC1zfcMu0Qb380J5mPUCAGh"
-
-os.environ["project"] = "zach-holloway/Stock-Price-Prediction"
-os.environ["api-token"] = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJmMzIzMDMyNi1jODBjLTQzZjUtODJjZC1kNTJkODFhMzg3MDUifQ=="
